636bc3d562a51e1102f76f3e787b5d27862ecf8c
Andrei Shablyka <kloppp0007@gmail.com>
2019-10-03 06:43:09 +0300

    feat: implement the getTimeForEducation function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+  focus = 'family',
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  let hours = (knowsProgramming) ? 800 : 1300;
+  return Math.ceil(hours / config[focus]);
+};
